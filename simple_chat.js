e46d3b63d4ed710fc881c32ac3741dc5f6b3231c
afnankheir <afnankheir@users.noreply.github.com>
2015-06-11 14:10:11 +0300

    Update simple_chat.js

--- simple_chat.js
+++ simple_chat.js
@@ -1,11 +1,34 @@
+messeges=new Mongo.Collection("messeges");
 // simple-todos.js
-if (Meteor.isClient) {
+
+  if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    msgs: [
-      { text: "This is message 1" },
-      { text: "This is message 2" },
-      { text: "This is messages 3" }
-    ]
+    msgs: function () {
+      return messeges.find({});
+    }
   });
+
+  Template.body.events({
+  "submit .new-msg": function (event) {
+    // This function is called when the new task form is submitted
+
+    var text = event.target.text.value;
+
+    messeges.insert({
+      text: text,
+      createdAt: new Date() // current time
+    });
+
+    // Clear form
+    event.target.text.value = "";
+
+    // Prevent default form submit
+    return false;
+  }
+});
+
+
+
 }
+
