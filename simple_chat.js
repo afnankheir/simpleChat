c4bddc249fc00dc372a57d6f0c7c2901d4258a90
afnankheir <afnanumatics@gmail.com>
2015-06-15 13:46:48 +0300

    publish users

--- simple_chat.js
+++ simple_chat.js
@@ -2,6 +2,7 @@ messeges=new Mongo.Collection("messeges");
 // simple-todos.js
 
   if (Meteor.isClient) {
+    Meteor.subscribe("userData");
   // This code only runs on the client
   Template.body.helpers({
     msgs: function () {
@@ -35,4 +36,15 @@ Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
 });
 
+}
+if (Meteor.isServer) {
+// server
+Meteor.publish("userData", function () {
+  if (this.userId) {
+    return Meteor.users.find({_id: this.userId},
+                             {fields: {'other': 1, 'things': 1}});
+  } else {
+    this.ready();
+  }
+});
 }
