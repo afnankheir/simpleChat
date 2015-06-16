7e5af9aa958838489df7030253cc550a513d0243
arwaahmed <arwa_tayeb92@hotmail.com>
2015-06-16 14:08:23 +0300

    Update simple_chat.js

--- simple_chat.js
+++ simple_chat.js
@@ -1,72 +1,24 @@
 messeges=new Mongo.Collection("messeges");
-// simple-todos.js
-
-
-if (Meteor.isClient) {
-  // This code only runs on the client
-  Meteor.subscribe("msgs");
-  Template.body.helpers({
-  msgs: function () {
-     
-    return messeges.find({});
-    }
-  });
-
-  Template.body.events({
+if(meteor.isClient)
+{
+   Meteor.publish("messeges",function(){
+     return Tasks.find();
+   }
+   );
   "submit .new-msg": function (event) {
-    // This function is called when the new task form is submitted
+        var text = event.target.text.value;
+        messeges.insert({
+        text: text,
+        createdAt: new Date(),
+        owner: Meteor.userId(),
+        username: Meteor.user().username 
+          });
+   Accounts.ui.config({
+       passwordSignupFields: "USERNAME_ONLY"
+     });
+   }
 
-    var text = event.target.text.value;
-
-    messeges.insert({
-      text: text,
-      createdAt: new Date(),
-        owner: Meteor.userId(),           // _id of logged in user
-  username: Meteor.user().username  // username of logged in user
-
-    });
-
-    // Clear form
-    event.target.text.value = "";
-
-    // Prevent default form submit
-    return false;
-  }
-  });
-  
-  Templete.chat.helpers({
-      isOwner : function (){
-         return this.owner==Meteor.userId();
-      }
-  });
-
-  Accounts.ui.config({
-    passwordSignupFields: "USERNAME_ONLY"
-  });
-  Meteor.subscribe("msgs");
-  setPrivate: function (chatId, setToPrivate) {
-  var msg = messeges.findOne();
-  }
-
-
-});
-meteor.methods(
-  messeges.update(chatId, { $set: { private: setToPrivate } });
-  Meteor.publish("messeges", function () {
-  return Tasks.find({
-    $or: [
-      { private: {$ne: true} },
-      { owner: this.userId }
-    ]
-  });
-  );
-if (Meteor.isServer) {
- Meteor.publish("msgs", function () {
-return Tasks.find({
-$or: [
-{ private: {$ne: true} },
-{ owner: this.userId }
-]
-});
-});
-}  
+   if(meteor.isServer)
+   {
+     Meteor.subscribe("messeges");
+   }
