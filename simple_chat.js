fcbd6e65d3b9c603f57630437e2aa7583a6da0b7
afnankheir <afnanumatics@gmail.com>
2015-06-18 11:10:51 +0300

    better code formatting

--- simple_chat.js
+++ simple_chat.js
@@ -6,60 +6,51 @@ messeges=new Mongo.Collection("messeges");
      Meteor.subscribe("messeges");
 
   // This code only runs on the client
-  Template.registerHelper('formatDate', function(date) {
-  return moment(date).fromNow();
 
-});
+    Template.registerHelper('formatDate', function(date) {
+      return moment(date).fromNow();
+      });
 
-  Template.body.helpers({
+    Template.body.helpers({
 
-    msgs: function () {
-     
+      msgs: function () {
      return messeges.find({});
     },
 
     chatUsers: function (){
-    //return userData.find({});
-
-  return Meteor.users.find({});
-}
-  });
-
-  Template.body.events({
-  "submit .new-msg": function (event) {
-    // This function is called when the new task form is submitted
-
-    var text = event.target.text.value;
-    var date = new Date;
-
-    messeges.insert({
-      text: text,
-      createdAt: date,
-        owner: Meteor.userId(),           // _id of logged in user
-  username: Meteor.user().username  // username of logged in user
-
+    return Meteor.users.find({});}
     });
 
-    // Clear form
-    event.target.text.value = "";
+    Template.body.events({
+     "submit .new-msg": function (event) {
+      var text = event.target.text.value;
+      var date = new Date;
+      messeges.insert({
+      text: text,
+      createdAt: date,
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
+      });
+      // Clear form
+      event.target.text.value = "";
+      // Prevent default form submit
+      return false;
+     }
+    });
+    
+    Accounts.ui.config({
+      passwordSignupFields: "USERNAME_ONLY"
+    });
+} // end of client 
 
-    // Prevent default form submit
-    return false;
-  }
-});
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+if (Meteor.isServer){
+  Meteor.publish("messeges",function(){
+  return messeges.find();
+  });
+  
+  Meteor.publish("userData", function () {
+ 
+  return Meteor.users.find({});
+  });
 
 }
-if (Meteor.isServer){
-   Meteor.publish("messeges",function(){
-     return messeges.find();
-   }
-   );
-Meteor.publish("userData", function () {
- 
-   return Meteor.users.find({});
-});
-
-  }
