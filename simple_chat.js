f7c96c70198047e6844c4e7064308d2fd4125245
arwaahmed <arwa_ahmed92@hotmail.com>
2015-06-23 11:46:40 +0300

    new room

--- simple_chat.js
+++ simple_chat.js
@@ -1,38 +1,93 @@
 messeges=new Mongo.Collection("messeges");
-// simple-todos.js
+typing= new Mongo.Collection ("typing");  
+  
+   Meteor.startup(function () {
+      typing.remove({});
+    
+    });
 
+ 
   if (Meteor.isClient) {
+    
+     Meteor.subscribe("userData");
+     Meteor.subscribe("typing")
+     Meteor.subscribe("messeges");
+
   // This code only runs on the client
-  Template.body.helpers({
+
+    Template.registerHelper('formatDate', function(date) {
+      return moment(date).fromNow();
+      });
+
+    Template.body.helpers({
+
     msgs: function () {
-     
      return messeges.find({});
+    
+    },
+
+    nowtyping: function(){
+      currentUserName= Meteor.user().username;
+      return typing.find({ name: { $ne: currentUserName}});
     }
   });
 
-  Template.body.events({
-  "submit .new-msg": function (event) {
-    // This function is called when the new task form is submitted
-
-    var text = event.target.text.value;
-
-    messeges.insert({
-      text: text,
-      createdAt: new Date(),
-        owner: Meteor.userId(),           // _id of logged in user
-  username: Meteor.user().username  // username of logged in user
-
-    });
-
-    // Clear form
-    event.target.text.value = "";
-
-    // Prevent default form submit
-    return false;
+Template.newRoom.helpers({
+  chatUsers: function (){
+    return Meteor.users.find({});
   }
-});
-Accounts.ui.config({
-  passwordSignupFields: "USERNAME_ONLY"
-});
+}); 
 
+    Template.body.events({
+       
+     "focus .new-msg": function (event){
+        usertyping = Meteor.user().username;
+        typeId= typing.insert ({name: usertyping});
+      },
+
+     "blur .new-msg": function (event){
+        typing.remove(typeId);
+      },
+
+
+     "submit .new-msg": function (event) {
+      typing.remove(typeId);
+      var text = event.target.text.value;
+      var date = new Date;
+      messeges.insert({
+      text: text,
+      createdAt: date,
+      owner: Meteor.userId(),           // _id of logged in user
+      username: Meteor.user().username  // username of logged in user
+      });
+      // Clear form
+      event.target.text.value = "";
+      // Prevent default form submit
+      return false;
+     },
+
+      chatUsers: function (){
+        return Meteor.users.find({});
+      }
+   
+    });
+    
+    Accounts.ui.config({
+      passwordSignupFields: "USERNAME_ONLY"
+    });
+} // end of client 
+
+if (Meteor.isServer){
+  Meteor.publish("messeges",function(){
+  return messeges.find();
+  });
+  
+  Meteor.publish("userData", function () {
+ 
+  return Meteor.users.find({});
+  });
+
+  Meteor.publish ("typing", function(){
+    return typing.find();
+  })
 }
